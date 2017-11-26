9a9426faec679c3199479a36ad0865dd3f855ea9
Mycroft1891 <mycroft1891@gmail.com>
2017-11-26 02:09:18 +0100

    Finish up Forms and Inputs

--- intro_to_backend/forms_and_inputs/README.md
+++ intro_to_backend/forms_and_inputs/README.md
@@ -1,5 +1,12 @@
 # Forms and Inputs
 
+1. [Getting Started](#start)
+2. [Install Flask and Setup](#setup)
+3. [Python Programming](#python)
+
+
+## <a name="start">Getting Started</a>
+
 - [Working File](index.html)
 
 Create a form
@@ -11,7 +18,8 @@ Create a form
 
 > Due personal reasons, I decided to use Flask instead of WebApp2 and Google App Engine.
 
-## Install Flask and setup
+
+## <a name="setup">Install Flask and setup</a>
 
 ```bash
 brew install python3
@@ -34,4 +42,67 @@ To run our flask app we execute this command (main.py is our app):
 
 ```bash
 FLASK_APP=main.py flask run
+```
+
+To create a route in Flaks we can use the `@app.route` decorator and specify a route as well as well as the methods we want to support:
+
+```python
+@app.route("/testform", methods=['GET', 'POST'])
+def testroute():
+    if valid_form:
+        return form_is_valid
+    else:
+        return form_is_invalid
+```
+
+To redirect from one route to another we can use the `redirect` and `url_for`. We have to import both before we can use them:
+
+```
+from flask import redirect
+from flask import url_for
+
+@app.route(/hello)
+def hello():
+  return redirect(url_for('bye'))
+
+@app.route(/bye)
+def bye():
+  return "Bye"
+```
+
+## <a name="python">Python Programming</a>
+
+```python
+######################################
+# PART 1: Check if string is digit   #
+######################################
+my_string = '10'
+my_string.isdigit() # True
+
+my_other_string = 'hello'
+my_other_string.isdigit() # False
+
+
+######################################
+# PART 2: Substitute String          #
+######################################
+sentence = "My name is %s"
+print(sentence %"Alexander") # My name is Alexander
+
+name = "Alexander"
+print(sentence %name) # My name is Alexander
+
+sentence = "My name is {name}"
+print(sentence.format(name=name)) # My name is Alexander
+
+
+######################################
+# PART 3: Escape HTML                #
+######################################
+for (i,o) in (#tuples of html tags and escaped versions):
+    tag = tag.replace(i, o)
+
+import cgi
+cgi.escape(tag, quote = True)
+
 ```
--- intro_to_backend/forms_and_inputs/main.py
+++ intro_to_backend/forms_and_inputs/main.py
@@ -1,24 +1,21 @@
 from flask import Flask
 from flask import request
 from flask import make_response
+from flask import redirect
+from flask import url_for
+import cgi
 
 app = Flask(__name__)
 
-form =  """
-        <br>
-        <form action="/testform">
-          <input type="text" name="day">
-          <input type="text" name="month">
-          <input type="text" name="year">
-          <br><br>
-          <input type="submit">
-        </form>
-        <hr>
-        """
+
+#########################
+# Create Routes         #
+#########################
 
 @app.route("/")
 def hello():
-    return form
+    return generate_form()
+
 
 @app.route("/testform", methods=['GET', 'POST'])
 def testroute():
@@ -27,34 +24,87 @@ def testroute():
     year = validate_year(request.args.get('year'))
 
     if day and month and year:
-        return "Thanks for submitting everything" + form
+        return redirect(url_for('thanks'))
     else:
-        return form
+        return generate_form("Please give valid dates", day, month, year)
+
+
+@app.route("/thanks")
+def thanks():
+    return  """
+            <h1 style="padding:5px; color:white; background-color:green;">
+                Thank you for submitting the form
+            </h1>
+            """
+
+
+#########################
+# Create Form           #
+#########################
+
+form =  """
+        <form action="/testform">
+            What is your birthday ?<br><br>
+            <label>Day<input type="text" name="day" value="{day}" placeholder="16"></label>
+            <label>Month <input type="text" name="month" value="{month}" placeholder="Aug" ></label>
+            <label>Year <input type="text" name="year" value="{year}" placeholder="1991"></label>
+            <br><br>
+            <input type="submit">
+        </form><hr>
+        <h3 style="color:white; background-color:red;">{message}</h3>
+        """
+
+
+#########################
+# Helper functions      #
+#########################
+
+def generate_form(message="", d="", m="", y=""):
+    return form.format(message=message, day=d, month=m, year=y)
+
+
+def escape_html(tag):
+    for (i,o) in (("&", "&amp;"), (">", "&gt;"), ("<", "&lt;"), ('"', "&quot;")):
+        tag = tag.replace(i, o)
+    # return cgi.escape(tag, quote = True)
 
 
 def validate_day(day):
     if day.isdigit() and int(day) > 0 and int(day) < 31:
         return int(day)
 
-def validate_month(month):
-    months = [
-        "January",
-        "February"
-        "March",
-        "April",
-        "May",
-        "June",
-        "July",
-        "August",
-        "September",
-        "October",
-        "November",
-        "December"
-    ]
 
-    if month.capitalize() in months:
-        return month.capitalize()
+def validate_month(month):
+    months = (
+        ("Jan", "January"),
+        ("Feb", "February"),
+        ("Mar", "March"),
+        ("Apr", "April"),
+        ("May", "May"),
+        ("Jun", "June"),
+        ("Jul", "July"),
+        ("Aug", "August"),
+        ("Sep", "September"),
+        ("Oct", "October"),
+        ("Nov", "November"),
+        ("Dec", "December")
+    )
+
+    month = month.capitalize()
+
+    for (abbr, full) in months:
+        if month == abbr or month == full:
+            return month
+
 
 def validate_year(year):
     if year.isdigit() and int(year) > 1900 and int(year) < 2020:
         return int(year)
+
+
+#########################
+# App Init              #
+#########################
+
+if __name__ == "__main__":
+    app.run(debug=True)
